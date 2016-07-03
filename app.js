ef0229953df5d1e0ad14c0ccfb50d3b138cf2d16
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2016-07-03 00:31:13 -0500

    adds express server

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var hi = 'hello world';
 
-if (true) {
-    console.log('hi');
-}
-console.log(hi);
+var app = express();
+
+var port = 5000;
+
+app.listen(port, function(err){
+    console.log('running server on port ' + port);
+});
