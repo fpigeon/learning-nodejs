47b52b4f4a58e4e1252ba9f1584412a44a389a9a
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2016-07-04 03:47:10 -0500

    adds mongodb and updates routes and views

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ var nav = [{
 }];
 
 var bookRouter = require('./src/routes/bookRoutes')(nav);
+var adminRouter = require('./src/routes/adminRoutes')(nav);
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
 
 app.get('/', function(req, res) {
     res.render('index', {
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,45 +1,46 @@
 var express = require('express');
-
 var bookRouter = express.Router();
-
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 var router = function(nav) {
-    var books = [{
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayenvich Tolstoy',
-        read: 'false'
-    }, {
-        title: 'Les Miserables',
-        genre: 'Historical Fiction',
-        author: 'Victor Hugo',
-        read: 'false'
-    }, {
-        title: 'War of the Worlds',
-        genre: 'Science Fiction',
-        author: 'H.G. Wells',
-        read: 'false'
-    }];
+
 
     bookRouter.route('/')
         .get(function(req, res) {
-            res.render('bookListView', {
-                title: 'Hello from Render',
-                nav: nav,
-                books: books
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function(err, db) {
+                var collection = db.collection('books');
+                collection.find({}).toArray(
+                    function(err, results) {
+                        res.render('bookListView', {
+                            title: 'Books',
+                            nav: nav,
+                            books: results
+                        });
+                    }
+                );
             });
         });
 
     bookRouter.route('/:id')
         .get(function(req, res) {
-            var id = req.params.id;
-            res.render('bookView', {
-                title: 'Hello from Render',
-                nav: nav,
-                book: books[id]
+            var id = new objectId(req.params.id);
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function(err, db) {
+                var collection = db.collection('books');
+                collection.findOne({_id: id},
+                    function(err, results) {
+                        res.render('bookView', {
+                            title: 'Book',
+                            nav: nav,
+                            book: results
+                        });
+                    }
+                );
             });
         });
 
     return bookRouter;
-}
+};
 
 module.exports = router;
